6126c8327851d3aebe2f7f2e6938a2440d5a3615
Jude Park <jude@judepark.com>
2019-07-20 22:16:24 -0400

    changes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,11 +4,11 @@ import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import { StaticQuery, graphql } from "gatsby"
 
-import logo from './../assets/img/max-logo.png'
-import favicon32 from './../assets/favicon/favicon-32x32.png'
-import favicon16 from './../assets/favicon/favicon-16x16.png'
-import faviconApple from './../assets/favicon/apple-touch-icon.png'
-import safariTab from './../assets/favicon/safari-pinned-tab.svg'
+import logo from './../assets/img/logo.png'
+import favicon32 from './../assets/img/logo.png'
+import favicon16 from './../assets/img/logo.png'
+import faviconApple from './../assets/img/logo.png'
+import safariTab from './../assets/img/logo.png'
 
 // Component imports
 import Header from '../components/header'
@@ -36,28 +36,28 @@ const Layout = ({ children, data }) => (
             <Helmet title={data.site.siteMetadata.title}>
                 <meta name="description" content={data.site.siteMetadata.description} />
                 <meta name="image" content={logo}/>
-                <meta itemprop="name" content="Max McKinney"/>
+                <meta itemprop="name" content="Jude Park"/>
                 <meta itemprop="description" content={data.site.siteMetadata.description}/>
                 <meta itemprop="image" content={data.site.siteMetadata.siteUrl + logo}/>
 
                 <meta name="twitter:card" content="summary"/>
-                <meta name="twitter:title" content="Max McKinney"/>
+                <meta name="twitter:title" content="Jude Park"/>
                 <meta name="twitter:description" content={data.site.siteMetadata.description}/>
-                <meta name="twitter:site" content="@timmaxmckinney"/>
+                <meta name="twitter:site" content="@judepark"/>
                 <meta name="twitter:image" content={data.site.siteMetadata.siteUrl + logo}/>
 
-                <meta name="og:title" content="Max McKinney"/>
+                <meta name="og:title" content="Jude Park"/>
                 <meta name="og:description" content={data.site.siteMetadata.description}/>
                 <meta name="og:image" content={data.site.siteMetadata.siteUrl + logo}/>
                 <meta name="og:url" content={data.site.siteMetadata.siteUrl}/>
-                <meta name="og:site_name" content="Max McKinney"/>
+                <meta name="og:site_name" content="Jude Park"/>
                 <meta name="og:locale" content="en_US"/>
                 <meta name="og:type" content="website"/>
 
                 <link rel="apple-touch-icon" sizes="180x180" href={faviconApple}/>
                 <link rel="icon" type="image/png" sizes="32x32" href={favicon32}/>
                 <link rel="icon" type="image/png" sizes="16x16" href={favicon16}/>
-                <link rel="mask-icon" href={safariTab} color="#5bbad5"/>
+                <link rel="mask-icon" href={safariTab} color="#ffcc00"/>
                 <meta name="msapplication-TileColor" content="#000000"/>
                 <meta name="theme-color" content="#ffffff"/>
 
